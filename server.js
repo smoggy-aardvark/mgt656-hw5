c17621c209bcf707a5f790b11ebb7650bcacbf17
Parvathi Letha <parvathi.letha@yale.edu>
2016-10-02 05:39:34 +0000

    Add nickname route

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ app.get('/', function(request, response){
     response.send('Hello MGT-656!');
 });
 app.get('/nickname', function(request, response){
-    response.send('smoggy-aardvark');
+    response.send('smoggy aardvark');
 });
 
 app.listen(process.env.PORT || 4000);
